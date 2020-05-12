c2ff7714bff19b2d885b5314eb38805573022d1f
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-12 07:24:33 +0100

    added contact form plugin

--- themes/jacoweb-freelance/assets/javascript/contact_me.js
+++ themes/jacoweb-freelance/assets/javascript/contact_me.js
@@ -1,25 +1,25 @@
-$(function() {
+$(function () {
 
-    $("input,textarea").jqBootstrapValidation({
+    $('input,textarea').jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function($form, event, errors) {
+        submitError: function ($form, event, errors) {
             // additional error messages or events
         },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
+        submitSuccess: function ($form, event) {
+            event.preventDefault() // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+            var name = $('input#name').val()
+            var email = $('input#email').val()
+            var phone = $('input#phone').val()
+            var message = $('textarea#message').val()
+            var firstName = name // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
+                firstName = name.split(' ').slice(0, -1).join(' ')
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: '././mail/contact_me.php',
+                type: 'POST',
                 data: {
                     name: name,
                     phone: phone,
@@ -27,44 +27,43 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
+                success: function () {
                     // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
+                    $('#success').html('<div class=\'alert alert-success\'>')
+                    $('#success > .alert-success').html('<button type=\'button\' class=\'close\' data-dismiss=\'alert\' aria-hidden=\'true\'>&times;')
+                        .append('</button>')
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append('<strong>Your message has been sent. </strong>')
                     $('#success > .alert-success')
-                        .append('</div>');
+                        .append('</div>')
 
                     //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#contactForm').trigger('reset')
                 },
-                error: function() {
+                error: function () {
                     // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
+                    $('#success').html('<div class=\'alert alert-danger\'>')
+                    $('#success > .alert-danger').html('<button type=\'button\' class=\'close\' data-dismiss=\'alert\' aria-hidden=\'true\'>&times;')
+                        .append('</button>')
+                    $('#success > .alert-danger').append('<strong>Sorry ' + firstName + ', it seems that my mail server is not responding. Please try again later!')
+                    $('#success > .alert-danger').append('</div>')
                     //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#contactForm').trigger('reset')
                 },
             })
         },
-        filter: function() {
-            return $(this).is(":visible");
+        filter: function () {
+            return $(this).is(':visible')
         },
-    });
+    })
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
-
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+})
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
-});
+$('#name').focus(function () {
+    $('#success').html('')
+})
